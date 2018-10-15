cb38fa5841e60bf910de9baa063d5a7f6aa0cc77
xuyong <xuyong@lsrobot.vip>
2018-10-15 11:42:44 +0800

    commit

---
+++ src/main/java/com/adam/testa/TestaApplication.java
@@ -0,0 +1,12 @@
+package com.adam.testa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestaApplication.class, args);
+	}
+}
